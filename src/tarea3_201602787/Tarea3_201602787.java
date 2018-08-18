30c754c7de8be61767e6b31ded3f4499efab8ee5
olive <olive@192.168.0.22>
2018-08-18 12:46:59 -0600

    Tarea 3 Inicializada

---
+++ src/tarea3_201602787/Tarea3_201602787.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea3_201602787;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author oliver
+ */
+public class Tarea3_201602787 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        
+        int respuesta;
+        
+        System.out.println("Menu de Operaciones");
+        System.out.println("1. Usuarios");
+        System.out.println("2. Contador de Números Repetidos");
+        System.out.println("3. Tres números de Mayor a Menor");
+        System.out.println("4. Calcular Promedio");
+        System.out.println("5. Salir");
+        
+        respuesta=entrada.nextInt();
+        
+        switch(respuesta){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;    
+        }
+        
+    }
+    
+}
